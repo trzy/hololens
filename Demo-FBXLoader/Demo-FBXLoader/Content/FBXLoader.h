2649e997da98d8eaadc5cd9122029efb8db09ec7
trzy <trzy@u.washington.edu>
2016-08-16 00:28:51 -0400

    Added comment describing how to package libfbxsdk.dll

--- Demo-FBXLoader/Demo-FBXLoader/Content/FBXLoader.h
+++ Demo-FBXLoader/Demo-FBXLoader/Content/FBXLoader.h
@@ -5,6 +5,31 @@
 #include "Common\DirectXHelper.h"
 #include <fbxsdk.h>
 
+/*
+ * FBX SDK DLL deployment in UWP apps:
+ * -----------------------------------
+ *
+ * These instructions pertain to v2016.1.2 of the FBX SDK.
+ *
+ * 1. Do not define FBXSDK_DLL. Although Autodesk claims this is required for
+ *    DLLs, it appears to blow up badly here.
+ * 2. Place the appropriate libfbxsdk.dll file into the project root (e.g., the
+ *    same folder as Package.appxmanifest. It will not work if placed anywhere
+ *    else, evidently.
+ * 3. In Solution Explorer, right click on the project, Add -> Existing Item...
+ * 4. Right click on the newly added file, select "Properties". Set "Content"
+ *    to "Yes". "Excluded From Build" will be blank and "Item Type" will be
+ *    "Does not participate in build".
+ * 5. Under Configuration Properties -> Linker -> Input -> Additional 
+ *    Dependencies, add libfbxsdk.lib, which is the dynamic version of the
+ *    library. For the static version, which does not require the DLL to be
+ *    bundled, use libfbxsdk-md.lib.
+ *
+ * Unresolved issues:
+ *
+ * 1. How to place the DLL file into an arbitrary sub-folder?
+ */
+
 class FBXLoader
 {
 public:
