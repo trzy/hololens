e035d6c5a455d8e15aecd8c9a4fb5743c78eb20e
trzy <trzy@u.washington.edu>
2016-08-18 23:08:39 -0400

    Helicopter now using HoloToolkit/Vertex Lit Configurable shaders. Moved spatial mapping renderer into own game object and wrote script to turn it off after 10 seconds.

---
+++ Demo-Holocopter/Assets/SpatialMap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA;
+using System.Collections;
+
+public class SpatialMap : MonoBehaviour
+{
+  public SpatialMappingRenderer m_renderer = null;
+
+  void Start()
+  {
+    m_renderer = GetComponent<SpatialMappingRenderer>();
+  }
+
+  void Update()
+  {
+    if (m_renderer.FreezeMeshUpdates)
+      return;
+    if (Time.time > 10.0f)
+      m_renderer.FreezeMeshUpdates = true;
+  }
+}
