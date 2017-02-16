cc8a10aecd86ef500992c5c8c6c3a9f73927ac57
trzy <trzy@u.washington.edu>
2017-02-15 22:22:01 -0500

    Converted helicopter script's variable names to camel case and fixed 3D audio for rotor sound

--- Demo-Holocopter/Assets/Scripts/Helicopter.cs
+++ Demo-Holocopter/Assets/Scripts/Helicopter.cs
@@ -6,10 +6,7 @@ using HoloLensXboxController;
 
 public class Helicopter: MonoBehaviour
 {
-  public Text                   m_ui_inputs = null;
-  public Text                   m_ui_control_program = null;
-
-  public Bullet                 m_bullet_prefab = null;
+  public Bullet bulletPrefab = null;
 
   public enum ControlMode
   {
@@ -38,21 +35,21 @@ public class Helicopter: MonoBehaviour
   }
 
   private ControllerInput m_xboxController = null;
-  private IEnumerator m_control_coroutine = null;
-  private IEnumerator m_rotor_speed_coroutine = null;
-  private ControlMode m_control_mode = ControlMode.Player;
-  private Controls m_player_controls = new Controls();
-  private Controls m_program_controls = new Controls();
-  private bool m_moving_last_frame = false;
-  private Vector3 m_joypad_lateral_axis;
-  private Vector3 m_joypad_longitudinal_axis;
-  private Vector3 m_target_forward;
-  private float m_gun_last_fired;
+  private IEnumerator m_controlCoroutine = null;
+  private IEnumerator m_rotorSpeedCoroutine = null;
+  private ControlMode m_controlMode = ControlMode.Player;
+  private Controls m_playerControls = new Controls();
+  private Controls m_programControls = new Controls();
+  private bool m_movingLastFrame = false;
+  private Vector3 m_joypadLateralAxis;
+  private Vector3 m_joypadLongitudinalAxis;
+  private Vector3 m_targetForward;
+  private float m_gunLastFired;
   private AutoAim m_autoAim = null;
   private Transform m_muzzleTransform = null;
 
-  private AudioSource m_gun_audio_source = null;
-  private AudioSource m_rotor_audio_source = null;
+  private AudioSource m_gunAudioSource = null;
+  private AudioSource m_rotorAudioSource = null;
 
   private const float SCALE = .06f;
   private const float MAX_TILT_DEGREES = 30;
@@ -65,16 +62,16 @@ public class Helicopter: MonoBehaviour
 
   private void SetControlProgram(IEnumerator coroutine)
   {
-    if (m_control_coroutine != coroutine)
-      StopCoroutine(m_control_coroutine);
-    m_control_coroutine = coroutine;
-    if (m_control_coroutine != null)
-      StartCoroutine(m_control_coroutine);
+    if (m_controlCoroutine != coroutine)
+      StopCoroutine(m_controlCoroutine);
+    m_controlCoroutine = coroutine;
+    if (m_controlCoroutine != null)
+      StartCoroutine(m_controlCoroutine);
   }
 
-  public void SetControlMode(ControlMode control_mode, IEnumerator coroutine = null)
+  public void SetControlMode(ControlMode controlMode, IEnumerator coroutine = null)
   {
-    m_control_mode = control_mode;
+    m_controlMode = controlMode;
     SetControlProgram(coroutine);
   }
 
@@ -83,59 +80,59 @@ public class Helicopter: MonoBehaviour
     return -(a.x * b.z - a.z * b.x);
   }
 
-  private float HeadingError(Vector3 target_forward)
+  private float HeadingError(Vector3 targetForward)
   {
-    Vector3 target = ProjectXZ(target_forward);
+    Vector3 target = ProjectXZ(targetForward);
     Vector3 forward = ProjectXZ(transform.forward);
     // Minus sign because error defined as how much we have overshot and need
     // to subtract, assuming positive rotation is clockwise.
     return -Mathf.Sign(CrossY(forward, target)) * Vector3.Angle(forward, target);
   }
 
-  private float HeadingErrorTo(Vector3 target_point)
+  private float HeadingErrorTo(Vector3 targetPoint)
   {
-    return HeadingError(target_point - transform.position);
+    return HeadingError(targetPoint - transform.position);
   }
 
-  private bool GoTo(Vector3 target_position)
+  private bool GoTo(Vector3 targetPosition)
   {
-    Vector3 to_target = target_position - transform.position;
+    Vector3 to_target = targetPosition - transform.position;
     float distance = Vector3.Magnitude(to_target);
-    float heading_error = HeadingErrorTo(target_position);
-    float abs_heading_error = Mathf.Abs(heading_error);
-    if (abs_heading_error > ACCEPTABLE_HEADING_ERROR)
-      m_program_controls.rotational = -Mathf.Sign(heading_error) * Mathf.Lerp(0.5F, 1.0F, Mathf.Abs(heading_error) / 360.0F);
+    float headingError = HeadingErrorTo(targetPosition);
+    float absHeadingError = Mathf.Abs(headingError);
+    if (absHeadingError > ACCEPTABLE_HEADING_ERROR)
+      m_programControls.rotational = -Mathf.Sign(headingError) * Mathf.Lerp(0.5F, 1.0F, Mathf.Abs(headingError) / 360.0F);
     else
-      m_program_controls.rotational = 0;
+      m_programControls.rotational = 0;
     if (distance > ACCEPTABLE_DISTANCE)
     {
       //TODO: reduce intensity once closer? Gradual roll-off within some event horizon.
-      Vector3 to_target_norm = to_target / distance;
-      m_program_controls.longitudinal = Vector3.Dot(to_target_norm, transform.forward);
-      m_program_controls.lateral = Vector3.Dot(to_target_norm, transform.right);
-      m_program_controls.altitude = to_target_norm.y;
+      Vector3 toTargetNorm = to_target / distance;
+      m_programControls.longitudinal = Vector3.Dot(toTargetNorm, transform.forward);
+      m_programControls.lateral = Vector3.Dot(toTargetNorm, transform.right);
+      m_programControls.altitude = toTargetNorm.y;
     }
     else
       return false;
     return true;
   }
 
-  private IEnumerator TraverseWaypointsCoroutine(List<GameObject> waypoints_param)
+  private IEnumerator TraverseWaypointsCoroutine(List<GameObject> waypointsParam)
   {
-    List<GameObject> waypoints = new List<GameObject>(waypoints_param);
+    List<GameObject> waypoints = new List<GameObject>(waypointsParam);
     foreach (GameObject waypoint in waypoints)
     {
       while (GoTo(waypoint.transform.position))
         yield return null;
     }
-    m_program_controls.Clear();
+    m_programControls.Clear();
   }
 
-  private IEnumerator FlyToPositionCoroutine(Vector3 target_position)
+  private IEnumerator FlyToPositionCoroutine(Vector3 targetPosition)
   {
-    while (GoTo(target_position))
+    while (GoTo(targetPosition))
       yield return null;
-    m_program_controls.Clear();
+    m_programControls.Clear();
   }
 
   public void TraverseWaypoints(List<GameObject> waypoints)
@@ -143,41 +140,41 @@ public class Helicopter: MonoBehaviour
     SetControlMode(ControlMode.Program, TraverseWaypointsCoroutine(waypoints));
   }
 
-  public void FlyToPosition(Vector3 target_position)
+  public void FlyToPosition(Vector3 targetPosition)
   {
-    SetControlMode(ControlMode.Program, FlyToPositionCoroutine(target_position));
+    SetControlMode(ControlMode.Program, FlyToPositionCoroutine(targetPosition));
   }
 
   private void FireGun(Vector3 aim)
   {
-    //Bullet bullet = Instantiate(m_bullet_prefab, transform.position + transform.up * -0.05f + transform.forward * 0.25f, Quaternion.identity) as Bullet;
-    Bullet bullet = Instantiate(m_bullet_prefab, m_muzzleTransform.position, Quaternion.identity) as Bullet;
+    //Bullet bullet = Instantiate(bulletPrefab, transform.position + transform.up * -0.05f + transform.forward * 0.25f, Quaternion.identity) as Bullet;
+    Bullet bullet = Instantiate(bulletPrefab, m_muzzleTransform.position, Quaternion.identity) as Bullet;
     bullet.transform.forward = aim; 
-    m_gun_last_fired = Time.time;
-    m_gun_audio_source.Play();
+    m_gunLastFired = Time.time;
+    m_gunAudioSource.Play();
   }
 
-  private IEnumerator ChangeRotorSpeedCoroutine(string rps_param_name, float target_rps, float ramp_time)
+  private IEnumerator ChangeRotorSpeedCoroutine(string rpsParamName, float targetRPS, float rampTime)
   {
     var animator = GetComponent<Animator>();
-    float start_rps = animator.GetFloat(rps_param_name);
-    float current_rps = start_rps;
-    float time_elapsed = 0.0F;
-    while (Mathf.Abs(current_rps - target_rps) > 0.0F)
+    float startRPS = animator.GetFloat(rpsParamName);
+    float currentRPS = startRPS;
+    float timeElapsed = 0.0F;
+    while (Mathf.Abs(currentRPS - targetRPS) > 0.0F)
     {
-      current_rps = start_rps + (target_rps - start_rps) * Mathf.Min(time_elapsed / ramp_time, 1.0F);
-      animator.SetFloat(rps_param_name, current_rps);
-      time_elapsed += Time.deltaTime;
+      currentRPS = startRPS + (targetRPS - startRPS) * Mathf.Min(timeElapsed / rampTime, 1.0F);
+      animator.SetFloat(rpsParamName, currentRPS);
+      timeElapsed += Time.deltaTime;
       yield return null;
     }
     //TODO: optimization: disable animation if rotation speed reaches 0
   }
 
-  private void ChangeRotorSpeed(ref IEnumerator coroutine, string rps_param_name, float target_rps, float ramp_time)
+  private void ChangeRotorSpeed(ref IEnumerator coroutine, string rpsParamName, float targetRPS, float rampTime)
   {
     if (coroutine != null)
       StopCoroutine(coroutine);
-    coroutine = ChangeRotorSpeedCoroutine(rps_param_name, target_rps, ramp_time);
+    coroutine = ChangeRotorSpeedCoroutine(rpsParamName, targetRPS, rampTime);
     StartCoroutine(coroutine);
   }
 
@@ -266,40 +263,40 @@ public class Helicopter: MonoBehaviour
      * causing the helicopter to travel faster forwards as it climbs.
      */
 
-    Controls controls = m_control_mode == ControlMode.Player ? m_player_controls : m_program_controls;
+    Controls controls = m_controlMode == ControlMode.Player ? m_playerControls : m_programControls;
     Rigidbody rb = GetComponent<Rigidbody>();
     Vector3 torque = new Vector3();
 
-    float current_heading_deg = heading;
-    Vector3 translational_input = new Vector3(controls.lateral, 0, controls.longitudinal);
-    Vector3 translational_force = Quaternion.Euler(new Vector3(0, current_heading_deg, 0)) * translational_input * SCALE * rb.mass * Mathf.Abs(Physics.gravity.y);
+    float currentHeadingDeg = heading;
+    Vector3 translationalInput = new Vector3(controls.lateral, 0, controls.longitudinal);
+    Vector3 translationalForce = Quaternion.Euler(new Vector3(0, currentHeadingDeg, 0)) * translationalInput * SCALE * rb.mass * Mathf.Abs(Physics.gravity.y);
 
-    float target_pitch_deg = Mathf.Sign(controls.longitudinal) * Mathf.Lerp(0, MAX_TILT_DEGREES, Mathf.Abs(controls.longitudinal));
-    float current_pitch_deg = Mathf.Sign(-transform.forward.y) * Vector3.Angle(transform.forward, ProjectXZ(transform.forward));
-    float target_roll_deg = Mathf.Sign(-controls.lateral) * Mathf.Lerp(0, MAX_TILT_DEGREES, Mathf.Abs(controls.lateral));
-    float current_roll_deg = Mathf.Sign(transform.right.y) * Vector3.Angle(transform.right, ProjectXZ(transform.right));
+    float targetPitchDeg = Mathf.Sign(controls.longitudinal) * Mathf.Lerp(0, MAX_TILT_DEGREES, Mathf.Abs(controls.longitudinal));
+    float currentPitchDeg = Mathf.Sign(-transform.forward.y) * Vector3.Angle(transform.forward, ProjectXZ(transform.forward));
+    float targetRollDeg = Mathf.Sign(-controls.lateral) * Mathf.Lerp(0, MAX_TILT_DEGREES, Mathf.Abs(controls.lateral));
+    float currentRollDeg = Mathf.Sign(transform.right.y) * Vector3.Angle(transform.right, ProjectXZ(transform.right));
 
-    float pitch_error_deg = target_pitch_deg - current_pitch_deg;
-    float roll_error_deg = target_roll_deg - current_roll_deg;
-    torque += SCALE * PITCH_ROLL_CORRECTIVE_TORQUE * new Vector3(pitch_error_deg, 0, roll_error_deg);
+    float pitchErrorDeg = targetPitchDeg - currentPitchDeg;
+    float rollErrorDeg = targetRollDeg - currentRollDeg;
+    torque += SCALE * PITCH_ROLL_CORRECTIVE_TORQUE * new Vector3(pitchErrorDeg, 0, rollErrorDeg);
     torque += SCALE * YAW_CORRECTIVE_TORQUE * controls.rotational * Vector3.up;
 
-    float up_world_local_deg = Vector3.Angle(Vector3.up, transform.up); // angle between world and local up
-    float hover_force = Mathf.Abs(rb.mass * Physics.gravity.y / Mathf.Cos(up_world_local_deg * Mathf.Deg2Rad));
-    float rotor_force = (controls.altitude + 1) * hover_force;
+    float upWorldLocalDeg = Vector3.Angle(Vector3.up, transform.up); // angle between world and local up
+    float hoverForce = Mathf.Abs(rb.mass * Physics.gravity.y / Mathf.Cos(upWorldLocalDeg * Mathf.Deg2Rad));
+    float rotorForce = (controls.altitude + 1) * hoverForce;
 
-    hover_force = Mathf.Abs(rb.mass * Physics.gravity.y);
-    rotor_force = SCALE * controls.altitude * rb.mass * Mathf.Abs(Physics.gravity.y);
+    hoverForce = Mathf.Abs(rb.mass * Physics.gravity.y);
+    rotorForce = SCALE * controls.altitude * rb.mass * Mathf.Abs(Physics.gravity.y);
 
     // Apply all forces
-    rb.AddRelativeForce(Vector3.up * rotor_force);
-    rb.AddForce(Vector3.up * hover_force);
-    rb.AddForce(translational_force);
+    rb.AddRelativeForce(Vector3.up * rotorForce);
+    rb.AddForce(Vector3.up * hoverForce);
+    rb.AddForce(translationalForce);
     rb.AddRelativeTorque(torque);
 
     // Pitch of engine is based on tilt
-    float engine_output = Mathf.Max(Mathf.Abs(controls.longitudinal), Mathf.Abs(controls.lateral));
-    m_rotor_audio_source.pitch = Mathf.Lerp(1.0f, 1.3f, engine_output);
+    float engineOutput = Mathf.Max(Mathf.Abs(controls.longitudinal), Mathf.Abs(controls.lateral));
+    m_rotorAudioSource.pitch = Mathf.Lerp(1.0f, 1.3f, engineOutput);
   }
 
   private void UpdateControls(Vector3 aim)
@@ -342,32 +339,32 @@ public class Helicopter: MonoBehaviour
 #endif
 
     // Any of the main axes (which are relative to orientation) pressed?
-    bool moving_this_frame = (hor != 0) || (ver != 0);
-    if (moving_this_frame && !m_moving_last_frame)
+    bool movingThisFrame = (hor != 0) || (ver != 0);
+    if (movingThisFrame && !m_movingLastFrame)
     {
       // Joypad was not pressed last frame, reorient based on current view position
-      m_joypad_lateral_axis = Vector3.Normalize(Camera.main.transform.right);
-      m_joypad_longitudinal_axis = Vector3.Normalize(Camera.main.transform.forward);
+      m_joypadLateralAxis = Vector3.Normalize(Camera.main.transform.right);
+      m_joypadLongitudinalAxis = Vector3.Normalize(Camera.main.transform.forward);
     }
-    m_moving_last_frame = moving_this_frame;
+    m_movingLastFrame = movingThisFrame;
 
     // Apply longitudinal and lateral controls. Compute projection of joypad
     // lateral/longitudinal axes onto helicopter's.
-    float joypad_longitudinal_to_heli_longitudinal = Vector3.Dot(m_joypad_longitudinal_axis, transform.forward);
-    float joypad_longitudinal_to_heli_lateral = Vector3.Dot(m_joypad_longitudinal_axis, transform.right);
-    float joypad_lateral_to_heli_longitudinal = Vector3.Dot(m_joypad_lateral_axis, transform.forward);
-    float joypad_lateral_to_heli_lateral = Vector3.Dot(m_joypad_lateral_axis, transform.right);
-    m_player_controls.longitudinal = joypad_longitudinal_to_heli_longitudinal * ver + joypad_lateral_to_heli_longitudinal * hor;
-    m_player_controls.lateral = joypad_longitudinal_to_heli_lateral * ver + joypad_lateral_to_heli_lateral * hor;
+    float joypadLongitudinalToHeliLongitudinal = Vector3.Dot(m_joypadLongitudinalAxis, transform.forward);
+    float joypadLongitudinalToHeliLateral = Vector3.Dot(m_joypadLongitudinalAxis, transform.right);
+    float joypadLateralToHeliLongitudinal = Vector3.Dot(m_joypadLateralAxis, transform.forward);
+    float joypadLateralToHeliLateral = Vector3.Dot(m_joypadLateralAxis, transform.right);
+    m_playerControls.longitudinal = joypadLongitudinalToHeliLongitudinal * ver + joypadLateralToHeliLongitudinal * hor;
+    m_playerControls.lateral = joypadLongitudinalToHeliLateral * ver + joypadLateralToHeliLateral * hor;
 
     // Helicopter rotation
-    m_player_controls.rotational = hor2;
+    m_playerControls.rotational = hor2;
 
     // Altitude control (trigger axes each range from 0 to 1)
-    m_player_controls.altitude = ver2;
+    m_playerControls.altitude = ver2;
 
     // Gun
-    if (fire && (Time.time - m_gun_last_fired >= GUN_FIRE_PERIOD))
+    if (fire && (Time.time - m_gunLastFired >= GUN_FIRE_PERIOD))
     {
       FireGun(aim);
     }
@@ -387,12 +384,12 @@ public class Helicopter: MonoBehaviour
 #if !UNITY_EDITOR
     m_xboxController = new ControllerInput(0, 0.19f);
 #endif
-    m_gun_audio_source = GetComponent<AudioSource>();
-    m_rotor_audio_source = transform.Find("RotorSound").gameObject.GetComponent<AudioSource>();
+    m_gunAudioSource = GetComponent<AudioSource>();
+    m_rotorAudioSource = transform.Find("RotorSound").gameObject.GetComponent<AudioSource>();
     SetControlMode(ControlMode.Program);
-    ChangeRotorSpeed(ref m_rotor_speed_coroutine, "RotorSpeed", 3, 0);
-    m_gun_last_fired = Time.time;
-    m_target_forward = transform.forward;
+    ChangeRotorSpeed(ref m_rotorSpeedCoroutine, "RotorSpeed", 3, 0);
+    m_gunLastFired = Time.time;
+    m_targetForward = transform.forward;
 
     // Find muzzle and auto aim component
     m_autoAim = GetComponentInChildren<AutoAim>();
@@ -410,48 +407,48 @@ public class Helicopter: MonoBehaviour
   {
 #if UNITY_EDITOR
     if (Input.GetKey("1"))
-      ChangeRotorSpeed(ref m_rotor_speed_coroutine, "RotorSpeed", 3, 5);
+      ChangeRotorSpeed(ref m_rotorSpeedCoroutine, "RotorSpeed", 3, 5);
     if (Input.GetKey("2"))
-      ChangeRotorSpeed(ref m_rotor_speed_coroutine, "RotorSpeed", 0, 5);
+      ChangeRotorSpeed(ref m_rotorSpeedCoroutine, "RotorSpeed", 0, 5);
     if (Input.GetKey(KeyCode.Z))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.Clear();
+      m_playerControls.Clear();
     }
     if (Input.GetKey(KeyCode.Comma))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.longitudinal = Mathf.Clamp(m_player_controls.longitudinal - 0.1F, -1.0F, 1.0F);
+      m_playerControls.longitudinal = Mathf.Clamp(m_playerControls.longitudinal - 0.1F, -1.0F, 1.0F);
     }
     if (Input.GetKey(KeyCode.Period))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.longitudinal = Mathf.Clamp(m_player_controls.longitudinal + 0.1F, -1.0F, 1.0F);
+      m_playerControls.longitudinal = Mathf.Clamp(m_playerControls.longitudinal + 0.1F, -1.0F, 1.0F);
     }
     if (Input.GetKey(KeyCode.Semicolon))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.lateral = Mathf.Clamp(m_player_controls.lateral - 0.1F, -1.0F, 1.0F);
+      m_playerControls.lateral = Mathf.Clamp(m_playerControls.lateral - 0.1F, -1.0F, 1.0F);
     }
     if (Input.GetKey(KeyCode.Quote))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.lateral = Mathf.Clamp(m_player_controls.lateral + 0.1F, -1.0F, 1.0F);
+      m_playerControls.lateral = Mathf.Clamp(m_playerControls.lateral + 0.1F, -1.0F, 1.0F);
     }
     if (Input.GetKey(KeyCode.LeftBracket))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.rotational = Mathf.Clamp(m_player_controls.rotational - 0.1F, -1, 1);
+      m_playerControls.rotational = Mathf.Clamp(m_playerControls.rotational - 0.1F, -1, 1);
     }
     if (Input.GetKey(KeyCode.RightBracket))
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.rotational = Mathf.Clamp(m_player_controls.rotational + 0.1F, -1, 1);
+      m_playerControls.rotational = Mathf.Clamp(m_playerControls.rotational + 0.1F, -1, 1);
     }
-    if (m_control_mode == ControlMode.Player)
+    if (m_controlMode == ControlMode.Player)
     {
       SetControlMode(ControlMode.Player);
-      m_player_controls.altitude = Mathf.Clamp(m_player_controls.altitude + Input.GetAxis("Mouse ScrollWheel"), -1, 1);
+      m_playerControls.altitude = Mathf.Clamp(m_playerControls.altitude + Input.GetAxis("Mouse ScrollWheel"), -1, 1);
     }
 #endif
   }
