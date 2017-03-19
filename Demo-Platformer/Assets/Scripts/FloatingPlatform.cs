86773cf94036e29f57ee1e05c584100fdb500230
trzy <trzy@u.washington.edu>
2017-03-19 04:14:10 -0400

    Sinusoidal motion parameterized by peak-to-peak amplitude and period

--- Demo-Platformer/Assets/Scripts/FloatingPlatform.cs
+++ Demo-Platformer/Assets/Scripts/FloatingPlatform.cs
@@ -2,9 +2,27 @@
 
 public class FloatingPlatform: MonoBehaviour
 {
+  [Tooltip("Distance traveled by the platform")]
+  public float peakToPeakAmplitude = 2;
+
+  [Tooltip("Time taken to cover the distance in seconds")]
+  public float period = 10;
+
+  private float m_amplitude;
+  private Vector3 m_centerPoint;
+  private float m_t0;
+
   private void Update()
   {
     // Platform surface normal is always +z (forward) axis
-    transform.position = transform.position + Time.deltaTime * transform.forward;
+    transform.position = m_centerPoint + m_amplitude * Mathf.Sin(2 * Mathf.PI * (Time.time - m_t0) / period - 0.5f * Mathf.PI) * transform.forward;
   }
+
+  private void Start()
+  {
+    m_amplitude = 0.5f * peakToPeakAmplitude;
+    m_centerPoint = transform.position + transform.forward * m_amplitude;
+    m_t0 = Time.time;
+  }
+
 }
