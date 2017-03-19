602e2c5d965d5247f616de4298a091b91e800234
trzy <trzy@u.washington.edu>
2017-03-19 04:16:31 -0400

    Added bottom parameters for moving platform, changed its dimensions, and reduced its selection pattern to 1 tile

--- Demo-Platformer/Assets/Scripts/GeoMaker.cs
+++ Demo-Platformer/Assets/Scripts/GeoMaker.cs
@@ -36,11 +36,15 @@ public class GeoMaker
   private float m_extrudeLength = 0;
   private float m_extrudeTime = 0;
   private float m_extrudeStart = 0;
+  //TODO: get rid of this and simply parameterize by tiles in texture map
   private Vector2[] m_simpleTopUV;
   private Vector2[] m_simpleSideUV;
   private Vector2[] m_cappedTopUV;
   private Vector2[] m_cappedCrownUV;
   private Vector2[] m_cappedBaseUV;
+  private Vector2[] m_movingTopUV;
+  private Vector2[] m_movingSideUV;
+  private Vector2[] m_movingBottomUV;
 
   private void CreateNewObject(Material material)
   {
@@ -62,6 +66,17 @@ public class GeoMaker
     m_platformType = type;
     CreateNewObject(material);
     m_selection.Reset();
+    switch (m_platformType)
+    {
+      case PlatformType.Raised:
+        m_selection.SetPattern(70, m_selection.defaultPattern);
+        m_extrudeLength = 0.3f;
+        break;
+      case PlatformType.Floating:
+        m_selection.SetPattern(1, new IVector3[] { new IVector3(0, 0) } );
+        m_extrudeLength = 0.03f;
+        break;
+    }
     m_state = State.Select;
     Debug.Log("Start selection");
   }
@@ -71,7 +86,6 @@ public class GeoMaker
     m_state = State.AnimatedExtrude;
     m_meshExtruder = new MeshExtruder(m_selection);
     m_extrudeMaterial = material;
-    m_extrudeLength = 0.3f;
     m_extrudeTime = 2;
     m_extrudeStart = Time.time;
     Debug.Log("Finish selection");
@@ -108,7 +122,7 @@ public class GeoMaker
       Vector2[] uv;
       //m_meshExtruder.ExtrudeSimple(out vertices, out triangles, out uv, extrudeLength, m_simpleTopUV, m_simpleSideUV, extrudeLength * 100);
       //m_meshExtruder.ExtrudeCapped(out vertices, out triangles, out uv, extrudeLength, m_cappedTopUV, m_cappedCrownUV, m_cappedBaseUV);
-      m_meshExtruder.ExtrudeSimpleWithBottom(out vertices, out triangles, out uv, extrudeLength, m_simpleTopUV, m_simpleSideUV, extrudeLength * 100);
+      m_meshExtruder.ExtrudeSimpleWithBottom(out vertices, out triangles, out uv, extrudeLength, m_movingTopUV, m_movingSideUV, m_movingBottomUV, extrudeLength * 100);
       m_mesh.Clear();
       m_mesh.vertices = vertices;
       m_mesh.uv = uv;
@@ -152,6 +166,8 @@ public class GeoMaker
       (1f / 128) * new Vector2(3.5f, 3.5f)
       
     };
+    
+    // Simple extrusions have 2 texture tiles (512x512) for a total of 512x1024
     m_simpleTopUV = new Vector2[]
     {
       new Vector2((1f/512) * 0.5f, (1f/1024) * (1024 - 0.5f)),
@@ -166,6 +182,8 @@ public class GeoMaker
       new Vector2((1f/512) * (512 - 0.5f), (1f/1024) * 0.5f),
       new Vector2((1f/512) * 0.5f, (1f/1024) * 0.5f)
     };
+
+    // Capped extrusions have 3 texture tiles for a total of 512x1536
     m_cappedTopUV = new Vector2[]
     {
       new Vector2((1f/512) * 0.5f, (1f/1536) * (1536 - 0.5f)),
@@ -186,6 +204,30 @@ public class GeoMaker
       new Vector2((1f/512) * (512 - 0.5f), (1f/1536) * (512 - 0.5f)),
       new Vector2((1f/512) * (512 - 0.5f), (1f/1536) * 0.5f),
       new Vector2((1f/512) * 0.5f, (1f/1536) * 0.5f)
+    };
+
+    // Simple extrusions with a bottom have 3 texture tiles (256x256) for a
+    // total of 256x768
+    m_movingTopUV = new Vector2[]
+    {
+      new Vector2((1f/256) * 0.5f, (1f/768) * (768 - 0.5f)),
+      new Vector2((1f/256) * (256 - 0.5f), (1f/768) * (768 - 0.5f)),
+      new Vector2((1f/256) * (256 - 0.5f), (1f/768) * (512 - 0.5f)),
+      new Vector2((1f/256) * 0.5f, (1f/768) * (512 - 0.5f))
+    };
+    m_movingSideUV = new Vector2[]
+    {
+      new Vector2((1f/256) * 0.5f, (1f/768) * (512 - 0.5f)),
+      new Vector2((1f/256) * (256 - 0.5f), (1f/768) * (512 - 0.5f)),
+      new Vector2((1f/256) * (256 - 0.5f), (1f/768) * (256 - 0.5f)),
+      new Vector2((1f/256) * 0.5f, (1f/768) * (256 - 0.5f))
+    };
+    m_movingBottomUV = new Vector2[]
+    {
+      new Vector2((1f/256) * 0.5f, (1f/768) * (256 - 0.5f)),
+      new Vector2((1f/256) * (256 - 0.5f), (1f/768) * (256 - 0.5f)),
+      new Vector2((1f/256) * (256 - 0.5f), (1f/768) * 0.5f),
+      new Vector2((1f/256) * 0.5f, (1f/768) * 0.5f)
     };
     m_selection = new PlanarTileSelection(70, selectUV);
   }
