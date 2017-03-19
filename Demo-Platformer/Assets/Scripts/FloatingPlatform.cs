c5700f95b0c6192bdb2f8d41436c4a5178c40948
trzy <trzy@u.washington.edu>
2017-03-18 22:57:12 -0400

    Floating platform controller

---
+++ Demo-Platformer/Assets/Scripts/FloatingPlatform.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FloatingPlatform: MonoBehaviour
+{
+  private void Update()
+  {
+    // Platform surface normal is always +z (forward) axis
+    transform.position = transform.position + Time.deltaTime * transform.forward;
+  }
+}
