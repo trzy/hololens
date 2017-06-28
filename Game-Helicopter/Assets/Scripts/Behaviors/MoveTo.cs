79d288ab41e2f45c3d0c037c5a16dd7a4f78efba
trzy <trzy@u.washington.edu>
2017-06-28 15:24:08 -0400

    Added callback for target reached

--- Game-Helicopter/Assets/Scripts/Behaviors/MoveTo.cs
+++ Game-Helicopter/Assets/Scripts/Behaviors/MoveTo.cs
@@ -8,10 +8,23 @@ using UnityEngine.AI;
 public class MoveTo: MonoBehaviour
 {
   private NavMeshAgent m_agent;
+  private Action m_OnTargetReached = null;
+  private float m_nearEnough = 0;
 
-  public void Move(Vector3 position)
+  public void Move(Vector3 position, Action OnTargetReached = null, float nearEnough = 0.1f)
   {
     m_agent.destination = position;
+    m_OnTargetReached = OnTargetReached;
+    m_nearEnough = nearEnough;
+  }
+
+  private void FixedUpdate()
+  {
+    if (!m_agent.pathPending && m_agent.remainingDistance <= m_nearEnough && m_OnTargetReached != null)
+    {
+      m_OnTargetReached();
+      m_OnTargetReached = null;
+    }
   }
 
   private void Awake()
