0c48c6406373e8246f1e6ccd12b85597d22cb567
trzy <trzy@u.washington.edu>
2017-07-29 16:12:43 -0400

    Added a note on TextMesh sizing

--- Game-Helicopter/Assets/Scripts/GameController.cs
+++ Game-Helicopter/Assets/Scripts/GameController.cs
@@ -1,4 +1,34 @@
-﻿using System.Collections;
+﻿/*
+ * Notes on TextMesh
+ * -----------------
+ * For lack of a better place to put this, a description of how to precisely
+ * control text size follows here. The discussion assumes character size is
+ * fixed to 1.
+ * 
+ * There are 2835 points per meter but Unity's default text height is 1 Unity
+ * unit (1 m) with the default font size of 13 pt (see 
+ * https://github.com/Microsoft/HoloToolkit-Unity/issues/331). Therefore, there
+ * should be
+ * 
+ *    1 / 2835 = 3.53e-4 m/pt 
+ *    
+ * in real-world units. But Unity's TextMesh by default is sized as
+ * 
+ *    1 / 13 = 7.69e-2 m/pt
+ *
+ * To scale down Unity's text to real-world units, apply the scale factor
+ * 
+ *    (1 / 2835) / (1 / 13) = 0.004585537918871252 ~= .005 = scale
+ *
+ * Additionally, for reasons unknown, it appears that each line occupies 1.5x
+ * the height that would be computed from font point size alone. 
+ * 
+ * To compute the height of a given line in meters:
+ * 
+ *    height = scale * (1 / 13) * (font size in points) * 1.5
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR.WSA;
