fc56ada29c0204c7049dd83f130e78c07910b76c
trzy <trzy@u.washington.edu>
2017-08-09 01:23:57 -0400

    Rotor script

---
+++ Game-Helicopter/Assets/Scripts/Agents/HelicopterRotor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterRotor: MonoBehaviour
+{
+  [Tooltip("Angular velocity in degrees/sec along each Euler axis.")]
+  public Vector3 angularVelocity = 360 * Vector3.up;
+
+  [Tooltip("Object to rotate about Y axis.")]
+  public Transform rotor;
+
+  private void Update()
+  {
+    rotor.Rotate(Time.deltaTime * angularVelocity);
+  }
+}
