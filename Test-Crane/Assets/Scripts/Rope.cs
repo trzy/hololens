9eae986c3a893653b9f7bf95976645d00f1816c1
trzy <trzy@u.washington.edu>
2017-08-16 21:50:19 -0400

    Add all assets

---
+++ Test-Crane/Assets/Scripts/Rope.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+  public Material material;
+
+  private void InitGameObject()
+  {
+    // Create reticle game object and mesh
+    gameObject.AddComponent<Cloth>();
+    SkinnedMeshRenderer meshRenderer = GetComponent<SkinnedMeshRenderer>();
+    meshRenderer.material = material;
+    meshRenderer.enabled = true;
+  }
+
+  private void Start()
+  {
+    InitGameObject();
+  }
+}
