06060a9e85c37d730ec1ee40e422eabbc7b7c956
trzy <trzy@u.washington.edu>
2017-09-16 01:32:49 -0400

    Aggressive fly over/under attack pattern working better now

--- Game-Helicopter/Assets/Scripts/Agents/HelicopterEnemy.cs
+++ Game-Helicopter/Assets/Scripts/Agents/HelicopterEnemy.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(HelicopterAutopilot))]
-public class HelicopterEnemy: MonoBehaviour
+public class HelicopterEnemy : MonoBehaviour
 {
   public Transform target;
 
   private HelicopterAutopilot m_autopilot;
+  private float m_boundingRadius;
 
   private enum State
   {
@@ -19,12 +20,140 @@ public class HelicopterEnemy: MonoBehaviour
   private State m_state = State.Thinking;
   bool m_engagingTarget = false;
 
+  private bool IsPathBlocked(Vector3 destination)
+  {
+    Vector3 toDestination = destination - transform.position;
+    float distance = toDestination.magnitude;
+    Ray ray = new Ray(transform.position + toDestination.normalized * m_boundingRadius, toDestination);
+    RaycastHit hit;
+    bool result = Physics.SphereCast(ray, m_boundingRadius, out hit, distance);
+    if (result)
+      Debug.Log("HIT " + hit.collider.name);
+    return result;
+    //return Physics.SphereCast(ray, m_boundingRadius, distance);
+  }
+
+  private float FindClearance(Vector3 position, Vector3 direction, float maxDistance = 4)
+  {
+    Ray ray = new Ray(target.position, direction);
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, maxDistance))
+      return (hit.point - target.position).magnitude;
+    return maxDistance;
+  }
+
+  private bool TryAttackPatternVerticalAndBehind(Vector3 toTarget, Vector3 verticalDirection)
+  {
+    float minDistanceVertical = 2 * m_boundingRadius; // minimum distance above/beneath target that must be clear
+    float minDistanceBehind = 2* m_boundingRadius;    // minimum distance behind the target that must be clear
+
+    Vector3[] positions = new Vector3[2];
+
+    // Do we have enough clearance directly above/below to fly through?
+    float clearance = FindClearance(transform.position, verticalDirection);
+    if (clearance < minDistanceVertical)
+    {
+      Debug.Log("VERTICAL: NO CLEARANCE: " + clearance);
+      return false;
+    }
+
+    // Choose a point halfway between the minimum vertical distance and
+    // clearance
+    float mid = 0.5f * (minDistanceVertical + clearance);
+    positions[0] = target.position + verticalDirection * mid;
+    if (IsPathBlocked(positions[0]))
+    {
+      Debug.Log("VERTICAL: PATH BLOCKED");
+      return false;
+    }
+
+    // Perform raycasts in a semicircle behind (and at same altitude as) target
+    Vector3 directionBehind = MathHelpers.Azimuthal(toTarget).normalized;
+    Vector3 bestDirection = Vector3.zero;
+    float bestClearance = 0;
+    for (float angle = -90; angle < 90; angle += 20)
+    {
+      Vector3 direction = Quaternion.Euler(angle * Vector3.up) * directionBehind;
+      clearance = FindClearance(target.position, direction);
+      if (clearance > bestClearance)
+      {
+        bestDirection = direction;
+        bestClearance = clearance;
+      }
+    }
+
+    // Check if sufficient room to go behind
+    if (bestClearance < minDistanceBehind)
+    {
+      Debug.Log("REAR: NO CLEARANCE: " + bestClearance);
+      return false;
+    }
+
+    // Determine point, ideally in between the min and max distance
+    mid = 0.5f * (minDistanceBehind + clearance);
+    positions[1] = target.position + bestDirection * mid;
+    if (IsPathBlocked(positions[1]))
+    {
+      Debug.Log("REAR: PATH BLOCKED");
+      return false;
+    }
+
+    Debug.Log("LAUNCHING ATTACK PATTERN");
+    m_autopilot.FollowPathAndLookAt(positions, target, 5);
+    return true;
+  }
+
+  bool TryAttackPatternUnderAndBehind(Vector3 toTarget)
+  {
+    return TryAttackPatternVerticalAndBehind(toTarget, -Vector3.up);
+  }
+
+  bool TryAttackPatternAboveAndBehind(Vector3 toTarget)
+  {
+    return TryAttackPatternVerticalAndBehind(toTarget, Vector3.up);
+  }
+
+  bool TryBackOffPattern(Vector3 toTarget)
+  {
+    //TODO: margin represents, roughly, the diameter of a bounding sphere
+    // around us. This should probably be computed at start up from the 
+    // collider footprint.
+    float margin = 0.75f;
+
+    // Measure clearance behind us
+    Vector3 back = -MathHelpers.Azimuthal(toTarget);
+    Ray ray = new Ray(transform.position, back);
+    RaycastHit hit;
+    float clearance = 0;
+    if (Physics.Raycast(ray, out hit, 10f))
+      clearance = (hit.point - transform.position).magnitude;
+    else
+      clearance = 10;
+
+    // Is there enough?
+    if (clearance - margin <= 0)
+      return false;
+
+    // If so, move back some random distance but at least halfway to maximum
+    float minDistance = 0.5f * (margin + (clearance - margin));
+    float maxDistance = clearance - margin;
+    float distance = UnityEngine.Random.Range(minDistance, maxDistance);
+    m_autopilot.FlyTo(transform.position + distance * back, target, 5);
+    return true;
+  }
+
+  bool TryMatchAltitudePattern(Vector3 toTarget)
+  {
+    return true;
+  }
+
   private void FixedUpdate()
   {
     if (target == null)
       return;
 
-    float distanceToTarget = (target.position - transform.position).magnitude;
+    Vector3 toTarget = target.position - transform.position;
+    float distanceToTarget = toTarget.magnitude;
 
     if (!m_engagingTarget)
     {
@@ -42,28 +171,41 @@ public class HelicopterEnemy: MonoBehaviour
     }
     else
     {
-      if (distanceToTarget < 1.5f && !m_autopilot.flying)
+      if (!m_autopilot.flying)
       {
-        // Circle the target when it is up close
-        //TODO: callback should take number of revolutions completed
-        m_autopilot.OrbitAndLookAt(target.transform, 0, 1.5f);
-        m_autopilot.throttle = 1;
+        // Not currently flying, select an attack pattern based on distance to target
+        if (distanceToTarget > 2.5f)
+        {
+          // Circle the target when it is up close
+          //TODO: callback should take number of revolutions completed
+          //m_autopilot.OrbitAndLookAt(target.transform, 0, 1.5f, 10);
+          //m_autopilot.throttle = 3;
+
+          TryAttackPatternAboveAndBehind(toTarget);
+          m_autopilot.throttle = 3f;
+
+          //TODO: additional attack patterns: strafe left/right (up/down?), circle about a point that is in front of target, random points  in front hemisphere of target
+          //TODO: If altitude mismatch, attempt correction by selecting a point on the semicircle directly in front of target (actually, on the side of the target closest
+          //      to us
+        }
+        else
+        {
+          // Attack patterns: back off, strafe
+          TryBackOffPattern(toTarget);
+        }
+      }
+      else
+      {
+        // Currently flying. Attack target and change flight patterns
+        // periodically.
       }
     }
-
-
-
 
     switch (m_state)
     {
       case State.Thinking:
-        
-        
-        
-        
-        
-        
-        
+
+
         /*
         if (distanceToTarget > 2)
         {
@@ -88,7 +230,7 @@ public class HelicopterEnemy: MonoBehaviour
         }
         */
 
-        
+
         m_state = State.FlyingTowards;
         break;
       case State.FlyingTowards:
@@ -107,5 +249,7 @@ public class HelicopterEnemy: MonoBehaviour
   private void Awake()
   {
     m_autopilot = GetComponent<HelicopterAutopilot>();
+    m_boundingRadius = Footprint.BoundingRadius(gameObject);
+    Debug.Log("Bounding radius: " + m_boundingRadius);
   }
 }
