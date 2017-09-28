5e33df8f987f30e956cda05afbd01b9e1284982f
trzy <trzy@u.washington.edu>
2017-09-28 01:03:12 -0400

    Forwards collision messages to another object (intended for colliders on children to notify parents of collision)

---
+++ Game-Helicopter/Assets/Scripts/CollisionForwarder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CollisionForwarder: MonoBehaviour
+{
+  [Tooltip("Component that will handle the collision. ")]
+  public MonoBehaviour collisionHandler;
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    // Do not forward to ourselves
+    if (collisionHandler != null && collisionHandler.gameObject != gameObject)
+      collisionHandler.SendMessage("OnCollisionEnter", collision);
+  }
+}
