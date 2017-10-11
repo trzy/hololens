cc7ecb01571ee083d80fcdb45fbd8515c9c5eae4
trzy <trzy@u.washington.edu>
2017-10-10 21:21:40 -0400

    AutoAim initial check-in

---
+++ Game-Helicopter/Assets/Scripts/Helicopter/AutoAim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AutoAim: MonoBehaviour
+{
+  [Tooltip("Muzzle point of gun.")]
+  public Transform muzzle;
+
+  [Tooltip("Targeting reticle object.")]
+  public GameObject targetingReticle;
+
+  [Tooltip("Default distance of reticle from muzzle when not locked onto a target.")]
+  public float defaultReticleDistance = 1;
+
+  private void Update()
+  {
+    targetingReticle.transform.position = muzzle.position + muzzle.forward * defaultReticleDistance;
+  }
+}
