59d30e74b39b368355ebcd1d2e6d447107d43e0c
trzy <trzy@u.washington.edu>
2017-10-13 00:47:43 -0400

    Create default and locked-on reticles procedurally

--- Game-Helicopter/Assets/Scripts/TargetingReticle.cs
+++ Game-Helicopter/Assets/Scripts/TargetingReticle.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 using HoloToolkit.Unity;
 
 [RequireComponent(typeof(Billboard))]
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
 public class TargetingReticle : MonoBehaviour
 {
-  public Color32 color;
+  public Material material;
   public float thickness = .0025f;
   public float radius = .02f;
 
@@ -21,39 +19,75 @@ public class TargetingReticle : MonoBehaviour
     set
     {
       if (!m_lockedOn && value)
+      {
         m_billboard.enabled = true;
+        m_defaultReticle.SetActive(false);
+        m_lockedReticle.SetActive(true);
+      }
       else if (m_lockedOn && !value)
+      {
+        m_defaultReticle.SetActive(true);
+        m_lockedReticle.SetActive(false);
         m_billboard.enabled = false;
+      }
       m_lockedOn = value;
     }
   }
 
   private MeshRenderer m_renderer;
   private Mesh m_mesh;
+  private GameObject m_defaultReticle;
+  private GameObject m_lockedReticle;
   private Billboard m_billboard;
   private bool m_lockedOn = false;
 
-  private void GenerateReticle(float radius, float thickness)
+  private void GenerateReticles(float radius, float thickness)
   {
+    Color32 unusedColor = new Color32();
+    Mesh mesh = null;
+    List<Vector3> verts = new List<Vector3>();
+    List<int> triangles = new List<int>();
+
+    m_defaultReticle = new GameObject("DefaultReticle");
+    m_defaultReticle.SetActive(true);
+    m_defaultReticle.transform.parent = transform;
+    m_defaultReticle.transform.localPosition = Vector3.zero;
+    m_defaultReticle.transform.localRotation = Quaternion.identity;
+    m_defaultReticle.AddComponent<MeshRenderer>().sharedMaterial = material;
+    mesh = m_defaultReticle.AddComponent<MeshFilter>().mesh;
+    verts.Clear();
+    triangles.Clear();
     float innerRadius = radius - 0.5f * thickness;
     float outerRadius = radius + 0.5f * thickness;
     int segments = 8;
-    List<Vector3> verts = new List<Vector3>();
-    List<int> triangles = new List<int>();
-    ProceduralMeshUtils.DrawArc(verts, triangles, null, color, color, innerRadius, outerRadius, 0, 360, segments);
-    m_mesh.vertices = verts.ToArray();
-    m_mesh.triangles = triangles.ToArray();
-    //m_mesh.colors32 = colors.ToArray();
-    m_mesh.RecalculateBounds();  // absolutely needed because Unity may erroneously think we are off-screen at times
+    ProceduralMeshUtils.DrawArc(verts, triangles, null, unusedColor, unusedColor, innerRadius, outerRadius, 0, 360, segments);
+    mesh.vertices = verts.ToArray();
+    mesh.triangles = triangles.ToArray();
+    mesh.RecalculateBounds();  // absolutely needed because Unity may erroneously think we are off-screen at times
+
+    m_lockedReticle = new GameObject("LockedReticle");
+    m_lockedReticle.SetActive(false);
+    m_lockedReticle.transform.parent = transform;
+    m_lockedReticle.transform.localPosition = Vector3.zero;
+    m_lockedReticle.transform.localRotation = Quaternion.identity;
+    m_lockedReticle.AddComponent<MeshRenderer>().sharedMaterial = material;
+    mesh = m_lockedReticle.AddComponent<MeshFilter>().mesh;
+    verts.Clear();
+    triangles.Clear();
+    ProceduralMeshUtils.DrawQuad(verts, triangles, null, unusedColor, +1 * Vector3.up * radius, thickness, radius);
+    ProceduralMeshUtils.DrawQuad(verts, triangles, null, unusedColor, -1 * Vector3.up * radius, thickness, radius);
+    ProceduralMeshUtils.DrawQuad(verts, triangles, null, unusedColor, +1 * Vector3.right * radius, radius, thickness);
+    ProceduralMeshUtils.DrawQuad(verts, triangles, null, unusedColor, -1 * Vector3.right * radius, radius, thickness);
+    mesh.vertices = verts.ToArray();
+    mesh.triangles = triangles.ToArray();
+    mesh.RecalculateBounds();
   }
 
   private void Awake()
   {
-    m_renderer = GetComponent<MeshRenderer>();
-    m_mesh = GetComponent<MeshFilter>().mesh;
     float thickness = .0025f;
     float radius = 0.02f;
-    GenerateReticle(radius, thickness);
+    GenerateReticles(radius, thickness);
     m_billboard = GetComponent<Billboard>();
   }
 }
